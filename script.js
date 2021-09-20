25e5cfffa9c7b0629c21eda3cfa42a37dfe91c8e
Vlada Plutalova <vladaplutalova@yandex.ru>
2021-09-20 13:08:28 +0300

    feat: smooth scrolling by anchors

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 let menuButton = document.querySelector(".menu-btn");
-let menu = document.querySelector('.menu');
+let menu = document.querySelector(".menu");
 
 menuButton.addEventListener("click", function () {
   menuButton.classList.toggle("active");
   menu.classList.toggle("active");
 });
 
-console.log('Здравствуй, уважаемый проверяющий!=) \n Самооценка CV: 145 / 150');
+let links = document.querySelectorAll('a[href*="#"]');
+
+for (let link of links) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const blockId = link.getAttribute("href");
+    document.querySelector("" + blockId).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+
+console.log("Здравствуй, уважаемый проверяющий!=) \n Самооценка CV: 145 / 150");
